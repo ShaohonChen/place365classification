086f71b153840a79f9b660caba431fb622ee69ed
shaohon <shaohon_chen@115lab.club>
2023-06-08 15:11:47 +0800

    完成了demo构建

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import paddle
 from paddle.vision import transforms
 import gradio as gr
-
+import numpy as np
 
 class Place365cls:
-    def __init__(self, model_pth='save/place365'):
+    def __init__(self, model_pth='./place365.pdparams'):
         # init model
-        network = paddle.vision.models.resnet50(pretrained=False)
-        network.fc = paddle.nn.Linear(2048, 365)
+        network = paddle.vision.models.resnet18(pretrained=False)
+        network.fc = paddle.nn.Linear(512, 365)
         model = paddle.Model(network)
         model.load(model_pth)
         self.model = model
+        self.sfm=paddle.nn.Softmax()
 
         # init transform
         self.transform = transforms.Compose([
@@ -34,22 +35,33 @@ class Place365cls:
         img = self.transform(img)
         img = img.unsqueeze(0)
         out = self.model.predict_batch(img)[0]
-        score, label = paddle.topk(out, k=5)
+        out = self.sfm(paddle.Tensor(out))
+        score, label = paddle.topk(out[0], k=5)
         return {self.id2categories_name[int(c)]: float(s) for c, s in zip(label, score)}
 
+
+"""
+        label = np.argsort(out)[-top_n:][::-1]
+        top_labels=dict()
+        for c in label:
+            c=int(c)
+            c_name=self.id2categories_name[c]
+            score=float(out[c])
+            top_labels[c_name]=score
+        return top_labels
+"""
 
 cls_model = Place365cls()
 
 
 def predict(image):
     result = cls_model.predict(image, top_n=5)
-    result_json = {item["label"]: float(item["score"]) for item in result}
-    return result_json
+    return result
 
 
 if __name__ == "__main__":
     interface = gr.Interface(fn=predict, inputs="image",
                              outputs=gr.Label(),
-                             title="ResNet50"
+                             title="Place365 classification"
                              )
     interface.launch()
